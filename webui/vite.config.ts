4c1ce407fde01af182b9ee96b4dc5b00a733dc39
Julian Waller <git@julusian.co.uk>
2025-10-23 23:47:33 +0100

    chore: fix local vite

--- webui/vite.config.ts
+++ webui/vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
 	plugins: [react(), tailwindcss()],
 	server: {
 		proxy: {
-			'/api': 'http://localhost:9999',
+			'/api': 'http://127.0.0.1:9999',
 		},
 	},
 	build: {
