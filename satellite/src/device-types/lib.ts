72d337e10a40ce15daee91c8343ff550927caacf
Julian Waller <git@julusian.co.uk>
2026-03-31 20:53:43 +0100

    cleanup

--- satellite/src/device-types/lib.ts
+++ satellite/src/device-types/lib.ts
@@ -1,54 +1,5 @@
-import * as imageRs from '@julusian/image-rs'
 import type { GridSize } from '../surfaceProxy.js'
 import type { SurfaceSchemaLayoutDefinition } from '@companion-surface/host'
-
-export interface TransformButtonImage {
-	buffer: Buffer
-	width: number
-	height: number
-	pixelFormat: imageRs.PixelFormat
-}
-
-/**
- * Transform a button image render to the format needed for a surface integration
- */
-export async function transformButtonImage(
-	rawImage: TransformButtonImage | undefined,
-	targetWidth: number,
-	targetHeight: number,
-	targetFormat: imageRs.PixelFormat,
-): Promise<Buffer> {
-	if (!rawImage) throw new Error('No input image provided')
-
-	const needsResize = rawImage.width !== targetWidth || rawImage.height !== targetHeight
-	if (!needsResize && targetFormat === rawImage.pixelFormat) return rawImage.buffer
-
-	let image = imageRs.ImageTransformer.fromBuffer(
-		rawImage.buffer,
-		rawImage.width,
-		rawImage.height,
-		rawImage.pixelFormat,
-	)
-
-	if (needsResize) {
-		image = image.scale(targetWidth, targetHeight, 'Fit')
-
-		// pad, in case a button is non-square
-		const dimensions = image.getCurrentDimensions()
-		const xOffset = (targetWidth - dimensions.width) / 2
-		const yOffset = (targetHeight - dimensions.height) / 2
-
-		image = image.pad(Math.floor(xOffset), Math.ceil(xOffset), Math.floor(yOffset), Math.ceil(yOffset), {
-			red: 0,
-			green: 0,
-			blue: 0,
-			alpha: 255,
-		})
-	}
-
-	const computedImage = await image.toBuffer(targetFormat)
-	return computedImage.buffer
-}
 
 export function calculateGridSize(surfaceLayout: SurfaceSchemaLayoutDefinition): GridSize {
 	return Object.values(surfaceLayout.controls).reduce(
